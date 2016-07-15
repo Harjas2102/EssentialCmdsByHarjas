73ddc80da4354ce86aa5026bbc8d6e420b86a9e5
Harjas2102 <harjassingh02@gmail.com>
2016-07-14 23:58:50 -0400

    eZ m8's call me mojang. jk I'm not affiliated with Mojang, pls dun't sue me.

---
+++ src/main/java/com/gmail/harjassingh02/JavaProgrammingTest/HarjasTestPlugin.java
@@ -0,0 +1,62 @@
+package com.gmail.harjassingh02.JavaProgrammingTest;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public class HarjasTestPlugin extends JavaPlugin{
+    @Override
+    public void onEnable() {
+        getCommand("Hello").setExecutor(new HelloCommand());
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (command.getName().equals("Bye")) {
+                p.kickPlayer("You have been kicked from the server");
+            } else if (command.getName().equals("Hello")) {
+                p.sendMessage(ChatColor.AQUA + "Hello " + p.getName() + " ! ");
+            }
+        }
+
+        else  {
+            if (command.getName().equals("Bye")) {
+                if (args.length > 0){
+                    List<Player> p= Bukkit.matchPlayer(args[0]);
+                    if (p.size()==0) {
+                        sender.sendMessage("That player is not online!");
+                        return true;
+                    }
+
+                    for (Player player : p){
+
+                        player.kickPlayer("You have been kicked from the server ");
+
+                    }
+
+
+                }
+            }
+
+            else if (command.getName().equals("Hello")) {
+                sender.sendMessage("Hello Console !");
+            }
+        }
+
+
+
+
+        return true;
+    }
+}
---
+++ src/main/java/com/gmail/harjassingh02/JavaProgrammingTest/HelloCommand.java
@@ -0,0 +1,23 @@
+package com.gmail.harjassingh02.JavaProgrammingTest;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HelloCommand implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            p.sendMessage(ChatColor.AQUA + "Hello " + p.getName() + " ! ");
+        } else
+
+            sender.sendMessage("Hello Console !");
+
+
+        return true;
+    }
+}
